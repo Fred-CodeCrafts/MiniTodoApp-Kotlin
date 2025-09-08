70baf1887cd2981a298448cf3c9d7176a08fb6e0
chmpgnsupernova <brianmariarvin@gmail.com>
2025-09-08 18:06:57 +0700

    TaskDelete + Confirmation + LifeCycle (REVISION)

--- app/src/main/java/com/fredcodecrafts/minitodoapp/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/fredcodecrafts/minitodoapp/ui/detail/DetailFragment.kt
@@ -1,21 +1,18 @@
 package com.fredcodecrafts.minitodoapp.ui.detail
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.fredcodecrafts.minitodoapp.data.Todo
 import com.fredcodecrafts.minitodoapp.data.TodoViewModel
 import com.fredcodecrafts.minitodoapp.databinding.FragmentDetailBinding
-import androidx.appcompat.app.AlertDialog
-import com.fredcodecrafts.minitodoapp.data.Todo
 
-
-/**
- * Shows selected Todo details. Data pulled from shared ViewModel.
- */
 class DetailFragment : Fragment() {
 
     private var _binding: FragmentDetailBinding? = null
@@ -41,12 +38,46 @@ class DetailFragment : Fragment() {
         binding.btnBack.setOnClickListener {
             findNavController().navigateUp()
         }
-
-
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("DetailFragment", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("DetailFragment", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("DetailFragment", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("DetailFragment", "onStop")
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
+        Log.d("DetailFragment", "onDestroyView")
         _binding = null
     }
-}
 
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("DetailFragment", "onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d("DetailFragment", "onDetach")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d("DetailFragment", "onSaveInstanceState")
+    }
+}
